ef8eafd3a6162daa72d8e4787c9d491340f01693
sakis5668 <sakis.tosounidis@gmail.com>
2025-01-26 09:31:05 +0200

    changed PORT to env var

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ app.use(
 );
 app.use(cookieParser());
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use("/snippet", require("./routers/snippetRouter"));
