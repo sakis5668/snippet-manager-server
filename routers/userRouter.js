1c5e368567484b1e096ff6f2415c5c71bb644b10
sakis5668 <sakis.tosounidis@gmail.com>
2025-01-26 12:20:20 +0200

    tweaked cookies

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -46,7 +46,11 @@ router.post("/", async (req, res) => {
     const token = jwt.sign(jwtData, process.env.JWT_SECRET);
 
     // send the token in a HTTP-only cookie
-    res.cookie("token", token, { httpOnly: true }).send();
+    res.cookie("token", token, { 
+        httpOnly: true, 
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : process.env.NODE_ENV === "production" && "none" , 
+        secure: process.env.NODE_ENV === "development" ? false : process.env.NODE_ENV === "production" && true 
+    }).send();
   } catch (error) {
     res.status(500).send();
   }
@@ -82,7 +86,11 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign(jwtData, process.env.JWT_SECRET);
 
     // send the token in a HTTP-only cookie
-    res.cookie("token", token, { httpOnly: true }).send();
+    res.cookie("token", token, { 
+        httpOnly: true, 
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : process.env.NODE_ENV === "production" && "none" , 
+        secure: process.env.NODE_ENV === "development" ? false : process.env.NODE_ENV === "production" && true 
+    }).send();
   } catch (error) {
     res.status(500).send();
   }
